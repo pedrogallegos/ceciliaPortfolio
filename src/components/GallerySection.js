3567a7c1c784507c23c0d749abef953f4ea217ee
Pedro Gallegos Garcilazo <pedrogallegos543@gmail.com>
2025-07-19 23:30:42 -0600

    feat: Add framer motion to header, Hero and Gallery sections

--- src/components/GallerySection.js
+++ src/components/GallerySection.js
@@ -2,7 +2,8 @@
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Navigation, Pagination, Autoplay } from 'swiper/modules'
 import Image from 'next/image'
-
+import { motion, useInView } from 'framer-motion'
+import { useRef } from 'react'
 // Import Swiper Styles
 import 'swiper/css'
 import 'swiper/css/navigation'
@@ -10,18 +11,25 @@ import 'swiper/css/pagination'
 
 export default function GallerySection () {
     // Data from example for gallery
+    const ref = useRef(null)
+    const isInView = useInView(ref, { once: true })
     const obras = [
-        { id: 1, titulo: "Paisajes Sereno", descripcion: "Acuarela sobre papel", imagen: "/images/obras/images.jpg"},
-        { id: 2, titulo: "Paisajes Sereno", descripcion: "Acuarela sobre papel", imagen: "/images/obras/images.jpg"},
+        { id: 1, titulo: "El dolor está en las cosas inofensivas", tecnica: "Acuarela sobre papel", medidas: " 70x100 cm", año:"2025", imagen: "/images/obras/el-dolor-está-cosas-inofensivas.jpg"},
+        { id: 2, titulo: "El dolor está en las cosas inofensivas: autorretrato funerario", tecnica: "Mixta, Xilografía y colografía con aditivas", medidas: "35x25 cm - 56x38 cm", año: "2025" , imagen: "/images/obras/dolor-está-cosas-inofensivas.jpg"},
         { id: 3, titulo: "Paisajes Sereno", descripcion: "Acuarela sobre papel", imagen: "/images/obras/images.jpg"},
         { id: 4, titulo: "Paisajes Sereno", descripcion: "Acuarela sobre papel", imagen: "/images/obras/images.jpg"},
     ]
     return (
         <section className='py-20 bg-white'>
             <div className='max-w-6xl mx-auto px-6'></div>
-            <h2 className='text-4xl font-bold text-gray-900 text-center mb-12'>
+            <motion.h2
+                ref={ref}
+                initial={{ opacity: 0, y: 50 }}
+                animate={ isInView ? { opacity: 1, y: 0 }: {opacity: 0, y: 50 }}
+                transition={{ duration: 1.2, ease: "easeOut" }}
+                className='text-4xl font-bold text-gray-900 text-center mb-12'>
                 Mis obras
-            </h2>
+            </motion.h2>
         <Swiper
             modules={[Navigation, Pagination, Autoplay]}
             spaceBetween={30}
@@ -36,7 +44,13 @@ export default function GallerySection () {
         >
             {obras.map((obra) => (
                 <SwiperSlide key={obra.id}>
-                    <div className='bg-acuarela-agua p-6 rounded-lg shadow-lg gallery-card'>
+                    <motion.div
+                        whileHover={{ scale: 1.05, y: -10 }}
+                        whileTap={{ scale: 0.95 }}
+                        initial={{ opacity: 0, y: 50 }}
+                        animate= {{ opacity: 1, y: 0}}
+                        transition= {{ duration: 0.5, delay: obra.id * 0.1}}
+                        className='bg-acuarela-agua p-6 rounded-lg shadow-lg gallery-card'>
                         <div className='h-64 rounded-lg mb-4 overflow-hidden'>
                             <Image
                                 src={obra.imagen}
@@ -46,11 +60,15 @@ export default function GallerySection () {
                                 className='w-full h-64 object-cover rounded-lg mb-4 gallery-image'
                             />
                         </div>
-                    <h3 className='text-xl font-semibold text-gray-800 mb-2'>
+                    <h3 className='text-xl font-semibold text-gray-800 mb-2 text-center'>
                         {obra.titulo}
                     </h3>
-                    <p className='text-gray-600'>{obra.descripcion}</p>
+                    <div className='text-center'>
+                        <h4 className='text-gray-700 font-semibold'>{obra.tecnica}</h4>
+                        <p className='text-gray-600'>{obra.medidas}</p>
+                        <p className='text-gray-500'>{obra.año}</p>
                     </div>
+                    </motion.div>
                 </SwiperSlide>
             ))}
 
